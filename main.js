b48d1ce13ae91ffc20f57a790cc328229dcdcd1a
mahmoudessam16 <mahmoudemourad29@gmail.com>
2024-02-19 13:27:19 +0300

    modify image root

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ questions.forEach(ques => {
     ques.addEventListener("click", function () {
         this.nextElementSibling.classList.toggle("showAnswer");
         if (this.nextElementSibling.classList.contains("showAnswer")) {
-            this.lastElementChild.src = "http://127.0.0.1:5500/assets/images/icon-minus.svg";
+            this.lastElementChild.src = "/assets/images/icon-minus.svg";
         } else {
-            this.lastElementChild.src = "http://127.0.0.1:5500/assets/images/icon-plus.svg";
+            this.lastElementChild.src = "/assets/images/icon-plus.svg";
         }
     })
 });
